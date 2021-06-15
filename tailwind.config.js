01ce3b6e409ac0ae7669fd6a1bf9eccc8fb067da
rajju <vajrachara@yahoo.com>
2021-06-15 05:27:17 +0100

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  purge: [],
+  purge: [
+    {
+      enabled: false, // Remove or change to `true` to enable purging.
+      mode: 'layers',
+      content: [
+        // Add any paths here that contain classes that need to be kept after
+        // purging, such as Twig templates and exported Drupal configuration
+        // files.
+        // '**/*.twig'
+      ]
+    }
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
